3b55efd30e1499c104c3848495e28c9f62b275f8
SimonGideon <simongideon918@gmail.com>
2022-11-02 15:19:54 +0300

    ADD stylings

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import toDos from './models/components.js';
-// image
-import logo from './images/icons8-disney-logo.svg';
-// styles
+// import styels
 import './styles/styles.css';
+const toDosDiv = document.getElementById('to-do-list-item');
+let toDosItems = (myToDos) => {
+  return `
+    <div class="toDo-item">
+    <span>
+        <input type="text" placeholder="${myToDos.description}">
+        <p class="speaker-name">${myToDos.description}</p>
+        <p class="speaker-profession">${myToDos.completed}</p>
+    </span>
+    </div>
+    `;
+}
+toDosDiv.innerHTML = `
+
+${toDos.map(toDosItems).join('')}
+
+`;
