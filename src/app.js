190d38cfb79dc375c060700a78ff10ce2171c300
SimonGideon <simongideon918@gmail.com>
2022-11-03 14:19:03 +0300

    ADD right dotted

--- src/app.js
+++ src/app.js
@@ -2,32 +2,12 @@ import toDos from './models/components.js';
 // import styels
 import './styles/styles.css';
 
-const body = document.querySelector('body');
-const todoList = document.createElement('div');
-todoList.classList.add('todo-list');
-todoList.innerHTML = `
-        <div class="todo-title">
-            <h1>Today's To Do</h1>
-            <span class=reload>&#x21bb;</span>
-        </div>
-        <ul class="input">
-            <input type="text" class="todo-input" placeholder="Add to your list">
-            <span class="enter-input">&#x2714;</span>
-        </ul>
-        <div id="to-do-list-item">
-        </div>
-        <div class="button">
-            <button type="reset" class="btn">Clear all completed</button>
-        </div>
-`;
-body.appendChild(todoList);
-
 const toDosDiv = document.getElementById('to-do-list-item');
 const toDosItems = (myToDos) => `
     <div class="toDo-item">
     <ul>
         <input type="checkbox">
-        <span>${myToDos.description}</span>
+        <span>${myToDos.description}</span><span id= "span">&#8942;</span>
         
     </ul>
     </div>
