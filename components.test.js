46e1f5bb2e81169a3ca580981d18c4c0a515e7c0
SimonGideon <simongideon918@gmail.com>
2022-11-10 20:20:47 +0300

    Initial commit

---
+++ components.test.js
@@ -0,0 +1,10 @@
+import { addTask, taskArr } from './src/models/util';
+
+test('New item added', () => {
+  addTask({
+    index: 1,
+    description: 'Read book',
+    completed: false,
+  });
+  expect(taskArr).toHaveLength(1);
+});
